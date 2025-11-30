439c29d6060450a2c9c877a0e1bc3a7bb28740ee
aravind <codewitharavind7559@gmail.com>
2025-11-30 08:09:25 +0530

    updated cors

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin: [process.env.CLIENT_URL, process.env.CLIENT_LOCAL],
+    origin: [process.env.CLIENT_URL, process.env.CLIENT_LOCAL, process.env.CLIPDROP_URL],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
