43356309f3a215b7155269de049addcaa08ea3b6
aravind <codewitharavind7559@gmail.com>
2025-11-30 08:19:08 +0530

    fixed the cors issue

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin: [process.env.CLIENT_URL, process.env.CLIENT_LOCAL, process.env.CLIPDROP_URL],
+    origin: process.env.CORS_URLS,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
