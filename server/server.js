2da54e8f925f57a3cee76cdbf5ad9da656886ea6
aravind <codewitharavind7559@gmail.com>
2025-11-30 07:40:39 +0530

    added cors

--- server/server.js
+++ server/server.js
@@ -11,7 +11,10 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin: [process.env.CLIENT_URL]
+    origin: [process.env.CLIENT_URL, process.env.CLIENT_LOCAL],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }))
 await connectDB()
 
